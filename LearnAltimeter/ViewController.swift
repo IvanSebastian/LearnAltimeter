c8b4933f4e7689d999b7c6bef041f3f260ca8100
Ivan Sebastian <ivansedbert@gmail.com>
2019-07-08 13:28:41 +0700

    Initial Commit

---
+++ LearnAltimeter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnAltimeter
+//
+//  Created by Ivan Sebastian on 08/07/19.
+//  Copyright © 2019 Vanski Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
